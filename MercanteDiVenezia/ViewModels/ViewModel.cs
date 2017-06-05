50ca9ad1af893bdf6715aa1b5ba97fb4b7ed3bb8
CybransLoyalist <nendorel@gazeta.pl>
2017-06-05 22:40:22 +0200

    dodanie podstawowych formularzy, mozliwosci otwoerania popupów itp

---
+++ MercanteDiVenezia/ViewModels/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MercanteDiVenezia.ViewModels
+{
+    public abstract class ViewModel //todo add uts
+    {
+        protected void ShowDialog<TView>(ViewModel viewModel) where TView : UserControl, new()
+        {
+            var view = new TView();
+            view.DataContext = viewModel;
+            var win = CreateWindowHostingViewModel(view, true);
+            win.ShowDialog();
+
+        }
+
+        private Window CreateWindowHostingViewModel(object viewModel, bool sizeToContent)
+        {
+            var contentUI = new ContentControl {Content = viewModel};
+            var dockPanel = new DockPanel();
+            dockPanel.Children.Add(contentUI);
+            Window hostWindow = new Window
+            {
+                IsEnabled = true,
+                Content = dockPanel
+            };
+
+            if (sizeToContent)
+            {
+                hostWindow.SizeToContent = SizeToContent.WidthAndHeight;
+            }
+
+            return hostWindow;
+        }
+    }
+}
