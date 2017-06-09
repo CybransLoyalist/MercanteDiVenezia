182163e3c8c059f189eb30321c7ae99c3e2cb9d2
CybransLoyalist <nendorel@gazeta.pl>
2017-06-09 20:58:08 +0200

    master branch commit

--- MercanteDiVenezia/ViewModels/MainWindowViewModel.cs
+++ MercanteDiVenezia/ViewModels/MainWindowViewModel.cs
@@ -12,5 +12,7 @@
         {
             AdminViewModel = adminViewModel;
         }
+
+        //master
     }
 }
---
+++ MercanteDiVeneziaTests/ViewModelTests.cs
@@ -0,0 +1,21 @@
+using MercanteDiVenezia;
+using MercanteDiVenezia.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace MercanteDiVeneziaTests
+{
+    [TestFixture]
+    public class ViewModelTests
+    { 
+        private ViewModel _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new AdminViewModel();
+        }
+
+        //sialalalalalalala
+    }
+}
